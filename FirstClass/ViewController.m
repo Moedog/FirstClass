69eaac7a542fb1537dbe3035e22d231d37ddae0d
James Moen <jamesmoen@yahoo.com>
2015-06-23 21:36:48 -0600

    InitialCommit
    FirstClass

--- FirstClass/ViewController.m
+++ FirstClass/ViewController.m
@@ -24,4 +24,32 @@
     // Dispose of any resources that can be recreated.
 }
 
+static CGFloat margin = 15;
+static NSString * const Command = @"command";
+static NSString * const Reference = @"reference";
+
+- (NSArray *)gitCommands {
+    return @[@{Command: @"git status", Reference: @": shows changed files"},
+             @{Command: @"git diff", Reference: @": shows actual changes"},
+             @{Command: @"git add .", Reference: @": adds changed files to the commit"},
+             @{Command: @"git commit -m \"notes\"", Reference: @": commits the changes"},
+             @{Command: @"git log", Reference: @": displays progress log"},
+             @{Command: @"git comment --amend", Reference: @": re-enter last commit message"}
+             ];
+}
+
+- (CGFloat)heightOfReferenceString:(NSString *)reference {
+    CGRect bounding = [reference boundingRectWithSize:CGSizeMake(self.view.frame.size.width - 2 * margin, 0)
+                                              options:NSStringDrawingUsesLineFragmentOrigin
+                                           attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:15]}
+                                              context:nil];
+    return bounding.size.height;
+    
+}
+
+UIScrollView *scrollView = [UIScrollView new];
+UIScrollView.frame = self.view.frame;
+[self.view addSubview:scrollView];
+scrollView alwaysBounceVertical = YES;
+
 @end
